50593c976b08a96c4cd69b149b1866cd6d8c837b
wangzhou666 <wdx6666666@gmail.com>
2015-12-08 01:08:39 -0600

    final version

--- storageManager/Disk.java
+++ storageManager/Disk.java
@@ -169,8 +169,8 @@ public class Disk implements Serializable {
   protected void incrementDiskTimer(int num_blocks) {
     if (Config.SIMULATED_DISK_LATENCY_ON) {
       try {
-        Thread.sleep((long)(avg_seek_time+avg_rotation_latency
-        					+avg_transfer_time_per_block*num_blocks));
+        Thread.sleep((long)((avg_seek_time+avg_rotation_latency
+        					+avg_transfer_time_per_block*num_blocks)/100));
       } catch (Exception e) {
           System.out.print(e);
       }
